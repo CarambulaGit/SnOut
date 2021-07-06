3a1e5b6ead0ca29d0ce531afdb905f098cb5ed58
Carambula <antibog6@gmail.com>
2021-07-06 21:53:43 +0300

    add Consts

---
+++ Assets/Project/Classes/Consts.cs
@@ -0,0 +1,9 @@
+﻿namespace Project.Classes {
+    public static class Consts {
+        #region Ball
+
+        public static float BALL_SPEED_FOR_BLOCK_DESTROY = 3f;
+
+        #endregion
+    }
+}
