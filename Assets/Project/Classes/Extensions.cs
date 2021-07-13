9857c1371692e00a44bf49c2390afff33035dfd1
Carambula <antibog6@gmail.com>
2021-07-13 21:09:17 +0300

    add some extensions

--- Assets/Project/Classes/Extensions.cs
+++ Assets/Project/Classes/Extensions.cs
@@ -19,6 +19,34 @@ namespace Project.Classes {
         public static T Last<T>(this List<T> list) {
             return list[list.Count - 1];
         }
+        
+        public static List<T> ToList<T>(this T[,] array)
+        {
+            var width = array.GetLength(0);
+            var height = array.GetLength(1);
+            var ret = new List<T>(width * height);
+            for (var i = 0; i < width; i++)
+            {
+                for (var j = 0; j < height; j++)
+                {
+                    ret.Add(array[i, j]);
+                }
+            }
+            return ret;
+        }
+
+        public static T[] ToArray<T>(this T[,] array) {
+            var width = array.GetLength(0);
+            var height = array.GetLength(1);
+            var ret = new T[width * height];
+            for (int i = 0, counter = 0; i < width; i++) {
+                for (var j = 0; j < height; j++ , counter++) {
+                    ret[counter] = array[i, j];
+                }
+            }
+
+            return ret;
+        }
 
         #endregion
 
