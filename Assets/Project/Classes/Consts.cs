f511bfed440a78f5cca5d84260a7834a985a1aa5
Carambula <antibog6@gmail.com>
2021-07-13 21:08:39 +0300

    Add new constants

--- Assets/Project/Classes/Consts.cs
+++ Assets/Project/Classes/Consts.cs
@@ -2,7 +2,22 @@
     public static class Consts {
         #region Ball
 
-        public static float BALL_SPEED_FOR_BLOCK_DESTROY = 3f;
+        public const float BALL_SPEED_FOR_BLOCK_DESTROY = 3f;
+
+        #endregion
+        
+        #region SceneNames
+
+        public const string GAME_SCENE_NAME = "Game";
+        public const string MAIN_MENU_SCENE_NAME = "MainMenu";
+
+        #endregion;
+
+        #region Tags
+
+        public const string GAME_CONTROLLER_TAG = "GameController";
+        public const string BALL_TAG = "Ball";
+        public const string INPUT_CONTROLLER_TAG = "InputController";
 
         #endregion
     }
