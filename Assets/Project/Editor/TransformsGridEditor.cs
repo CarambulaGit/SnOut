9b7883c4be22970cf3446e257e2d3130f9741cd5
Carambula <antibog6@gmail.com>
2021-07-13 21:13:05 +0300

    add TransformsGrid

---
+++ Assets/Project/Editor/TransformsGridEditor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Project.Scripts;
+using UnityEditor;
+using UnityEngine;
+
+namespace Project.Editor {
+    [CustomEditor(typeof(TransformsGrid))]
+    class TransformsGridEditor : UnityEditor.Editor {
+        private TransformsGrid _grid;
+        private SerializedProperty _rowCount;
+        private SerializedProperty _columnCount;
+        private SerializedProperty _alignment;
+        // private int _prevAlignmentIndex;
+
+        private void OnEnable() {
+            Initialize();
+        }
+
+        private void Initialize() {
+            _rowCount = serializedObject.FindProperty("rowCount");
+            _columnCount = serializedObject.FindProperty("columnCount");
+            _alignment = serializedObject.FindProperty("alignment");
+            _grid = target as TransformsGrid;
+        }
+
+        private void OnValidate() {
+            // GridSizeCheck();
+            // AlignmentCheck();
+        }
+
+        // private void GridSizeCheck() {
+        //     if (_grid.content.Count <= _rowCount.intValue * _columnCount.intValue) return;
+        //     var needToAdd = _grid.content.Count - _rowCount.intValue * _columnCount.intValue;
+        //     _rowCount.intValue += needToAdd % _columnCount.intValue != 0
+        //         ? needToAdd / _columnCount.intValue + 1
+        //         : needToAdd / _columnCount.intValue;
+        //     serializedObject.ApplyModifiedProperties();
+        // }
+        //
+        //
+        // private void AlignmentCheck() {
+        //     if (_alignment.enumValueIndex == _prevAlignmentIndex) return;
+        //     Debug.Log("Alignment changed");
+        //     _grid.OnAlignChanged();
+        //     _prevAlignmentIndex = _alignment.enumValueIndex;
+        // }
+
+        public override void OnInspectorGUI() {
+            // if (GUILayout.Button("Row and column count depends on content")) {
+            // _rowCount.intValue = _grid.content.GetLength(0);
+            // _columnCount.intValue = _grid.content.GetLength(1);
+            // serializedObject.ApplyModifiedProperties();
+            // }
+
+            DrawDefaultInspector();
+        }
+    }
+}
---
+++ Assets/Project/Scripts/TransformsGrid.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project.Scripts {
+    public class TransformsGrid : MonoBehaviour {
+        public enum Alignment {
+            UpperLeft,
+            Central
+        }
+
+        public int rowCount;
+        public int columnCount;
+        public Vector2 cellSize;
+        public Alignment alignment;
+        public List<Transform> content = new List<Transform>();
+        private Action UpdatePosition;
+        private Alignment _prevAlignment;
+        
+        private void OnEnable() {
+            OnAlignChanged();
+            _prevAlignment = alignment;
+        }
+
+        private void OnValidate() {
+            GridSizeCheck();
+            AlignmentCheck();
+            UpdatePosition?.Invoke();
+        }
+
+        private void GridSizeCheck() {
+            if (content.Count <= rowCount * columnCount) return;
+            var needToAdd = content.Count - rowCount * columnCount;
+            rowCount += needToAdd % columnCount != 0
+                ? needToAdd / columnCount + 1
+                : needToAdd / columnCount;
+        }
+        
+        private void AlignmentCheck() {
+            if (alignment == _prevAlignment) return;
+            OnAlignChanged();
+            _prevAlignment = alignment;
+        }
+
+        private void OnAlignChanged() {
+            UpdatePosition = alignment switch {
+                Alignment.UpperLeft => UpdatePositionUpperLeft,
+                Alignment.Central => UpdatePositionCentral,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        public void SetContent(Transform[,] array) {
+            content.Clear();
+            rowCount = array.GetLength(0);
+            columnCount = array.GetLength(1);
+            foreach (var elem in array) {
+                content.Add(elem);
+            }
+            UpdatePosition.Invoke();
+        }
+
+        private void UpdatePositionCentral() => UpdatePositionByParent(columnCount / 2f, rowCount / 2f);
+        private void UpdatePositionUpperLeft() => UpdatePositionByParent(0, 0);
+
+        private void UpdatePositionByParent(float parentPosInGridX, float parentPosInGridY) {
+            for (var i = 0; i < content.Count; i++) {
+                try {
+                    if (content[i] is null) continue;
+                    var _ = content[i].hierarchyCapacity;
+                }
+                catch (Exception e) {
+                    continue;
+                }
+                // todo remove this ****
+
+                var y = i / columnCount + 0.5f;
+                var x = i % columnCount + 0.5f;
+                var offsetY = y - parentPosInGridY;
+                var offsetX = x - parentPosInGridX;
+                content[i].localPosition = new Vector3(offsetX * cellSize.x, -offsetY * cellSize.y);
+            }
+        }
+    }
+}
