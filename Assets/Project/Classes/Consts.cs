dacc3647df553a4c06e1d401093ab590a7fb36f7
Carambula <antibog6@gmail.com>
2021-07-16 01:46:24 +0300

    add new constants

--- Assets/Project/Classes/Consts.cs
+++ Assets/Project/Classes/Consts.cs
@@ -1,11 +1,17 @@
 ﻿namespace Project.Classes {
     public static class Consts {
+        #region GameSettings
+
+        public const int MAX_FPS = 45;
+
+        #endregion
+
         #region Ball
 
         public const float BALL_SPEED_FOR_BLOCK_DESTROY = 3f;
 
         #endregion
-        
+
         #region SceneNames
 
         public const string GAME_SCENE_NAME = "Game";
@@ -18,6 +24,7 @@
         public const string GAME_CONTROLLER_TAG = "GameController";
         public const string BALL_TAG = "Ball";
         public const string INPUT_CONTROLLER_TAG = "InputController";
+        public const string GRID = "Grid";
 
         #endregion
     }
