8505a59af54ec59074d5b2b075fb10414f584d85
Carambula <antibog6@gmail.com>
2021-07-16 01:55:53 +0300

    add SnakeBlockPool

---
+++ Assets/Project/Scripts/SnakeBlockPool.cs
@@ -0,0 +1,8 @@
+﻿namespace Project.Scripts {
+    public class SnakeBlockPool : Pool<SnakeBlockView> {
+        public override void ReturnObject(SnakeBlockView obj) {
+            obj.Unconnect();
+            base.ReturnObject(obj);
+        }
+    }
+}
