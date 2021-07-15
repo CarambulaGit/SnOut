8d87ea8e1884cc1352f910aef225be7b097b8cd0
Carambula <antibog6@gmail.com>
2021-07-16 01:55:42 +0300

    add SnakeBlockView

---
+++ Assets/Project/Scripts/SnakeBlockView.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Project.Classes;
+using UnityEngine;
+
+namespace Project.Scripts {
+    public class SnakeBlockView : MonoBehaviour {
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        [SerializeField] private Sprite spriteDefault;
+        [SerializeField] private Sprite spriteHead;
+
+        private TransformsGrid _grid;
+        private GameController _gameController;
+
+        public int SnakeBlockIndex { get; private set; } = -1;
+        public Snake Snake { get; private set; }
+        public Snake.SnakeBlock SnakeBlock => Snake.SnakeBlocks[SnakeBlockIndex];
+        public bool Connected { get; private set; }
+
+        private void Awake() {
+            _gameController = GameObject.FindWithTag(Consts.GAME_CONTROLLER_TAG).GetComponent<GameController>();
+            _grid = GameObject.FindWithTag(Consts.GRID).GetComponent<TransformsGrid>();
+            transform.localScale *= spriteRenderer.bounds.size.x / _gameController.CellSize;
+        }
+
+        private void Start() {
+            _gameController.GameManager.OnTickEnd += UpdatePosition;
+        }
+
+        private void UpdateSprite() {
+            spriteRenderer.sprite = SnakeBlockIndex == 0 ? spriteHead : spriteDefault;
+        }
+
+        public void ConnectToSnakeBlock(int snakeBlockIndex, Snake snake) {
+            Snake = snake;
+            SnakeBlockIndex = snakeBlockIndex;
+            UpdateSprite();
+            Connected = true;
+            UpdatePosition();
+        }
+
+        public void Unconnect() {
+            SnakeBlockIndex = -1;
+            Snake = null;
+            Connected = false;
+        }
+
+        private void UpdatePosition() {
+            if (!Connected) return;
+            transform.position = _grid.GetPositionByXAndY(SnakeBlock.X, SnakeBlock.Y);
+        }
+    }
+}
