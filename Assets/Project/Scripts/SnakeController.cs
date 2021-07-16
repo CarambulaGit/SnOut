3339c7d4361643e44947a3bbf1ec04186c03ce83
Carambula <antibog6@gmail.com>
2021-07-16 22:49:33 +0300

    add logic for SnakeController

--- Assets/Project/Scripts/SnakeController.cs
+++ Assets/Project/Scripts/SnakeController.cs
@@ -1,9 +1,26 @@
+using System;
+using Project.Classes;
 using UnityEngine;
 
 namespace Project.Scripts {
     public class SnakeController : MonoBehaviour {
-        void Start() { }
+        [SerializeField] private InputController inputController;
+        public Snake.Direction CurrentDirection { get; private set; }
+        private void Awake() {
+            if (inputController == null) {
+                inputController = GameObject.FindWithTag(Consts.INPUT_CONTROLLER_TAG).GetComponent<InputController>();
+            }
+        }
 
-        void Update() { }
+        void Update() {
+            CurrentDirection = inputController.GetSwipe() switch {
+                InputController.SwipeType.None => CurrentDirection,
+                InputController.SwipeType.Left => Snake.Direction.Left,
+                InputController.SwipeType.Right => Snake.Direction.Right,
+                InputController.SwipeType.Up => Snake.Direction.Up,
+                InputController.SwipeType.Down => Snake.Direction.Down,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
     }
 }
